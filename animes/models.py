db3fc51d40c76dc292103cc8e7f621b03a1aea8a
Somron Khabibov <habibov200@gmail.com>
2022-05-01 18:27:08 +0300

    added animes app

---
+++ animes/models.py
@@ -0,0 +1,129 @@
+from datetime import date
+
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    """Категория"""
+    name = models.CharField("Категория", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категория"
+
+
+class Character(models.Model):
+    """Персонажи и Авторы"""
+    name = models.CharField("Имя", max_length=100)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="characters/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Персонажи и Авторы"
+        verbose_name_plural = "Персонажи и Авторы"
+
+
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField("Имя", max_length=150)
+    description = models.TextField("Описание")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанры"
+        verbose_name_plural = "Жанры"
+
+
+class Anime(models.Model):
+    """Аниме"""
+    title = models.CharField("Название", max_length=100)
+    description = models.TextField("Описание")
+    poster = models.ImageField("Постер", upload_to="anime/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=1990)
+    author = models.ManyToManyField(Character, verbose_name="Автор", related_name="anime_author")
+    characters = models.ManyToManyField(Character, verbose_name="Персонажи", related_name="anime_characters")
+    genres = models.ManyToManyField(Genre, verbose_name="Жанры")
+    world_premiere = models.DateField("Примьера в мире", default=date.today)
+    budget = models.PositiveIntegerField("Бюджет", default=0, help_text="указывать сумму в долларах")
+    fees_in_japan = models.PositiveIntegerField("Сборы в Японии", default=0, help_text="указывать сумму в долларах")
+    fees_in_world = models.PositiveIntegerField("Сборы в мире", default=0, help_text="указывать сумму в долларах")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=160, unique=True)
+    draft = models.BooleanField("Черновик", default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Аниме"
+        verbose_name_plural = "Аниме"
+
+
+class AnimeShots(models.Model):
+    """Кадры из аниме"""
+    title = models.CharField("Заголовок", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="anime_shots/")
+    anime = models.ForeignKey(Anime, verbose_name="Аниме", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Кадры из аниме"
+        verbose_name_plural = "Кадры из аниме"
+
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.SmallIntegerField("Значение", default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "Звезда рейтинга"
+        verbose_name_plural = "Звезда рейтинга"
+
+
+class Rating(models.Model):
+    """Рейтинг"""
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    anime = models.ForeignKey(Anime, on_delete=models.CharField, verbose_name="аниме")
+
+    def __str__(self):
+        return f"{self.star} - {self.anime}"
+
+    class Meta:
+        verbose_name = "Рейтинг"
+        verbose_name_plural = "Рейтинг"
+
+
+class Reviews(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Сообщение", max_length=5000)
+    parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True)
+    anime = models.ForeignKey(Anime, verbose_name="аниме", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.anime}"
+
+    class Meta:
+        verbose_name = "Отзывы"
+        verbose_name_plural = "Отзывы"
